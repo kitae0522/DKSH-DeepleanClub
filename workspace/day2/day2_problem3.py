f3d9d22a1f04f313e3bb793004eda63caf14dbf5
Song Kitae <40349572+kitae0522@users.noreply.github.com>
2020-04-12 11:56:54 +0900

    Last Update 200412

---
+++ workspace/day2/day2_problem3.py
@@ -0,0 +1,14 @@
+def palindrome(word):
+
+    for i in range(len(word) // 2):  # 문자열 길이의 반만큼 반복
+        if word[i] != word[-1 - i]:  # 왼쪽 문자와 오른쪽 문자를 비교하여 문자가 다르면
+            return False  # 팰린드롬 문자가 아니다
+        else:
+            return True  # 팰린드롬 문자이다
+
+
+if __name__ == '__main__':
+
+    word = "level1"
+    res = palindrome(word)
+    print(res)
