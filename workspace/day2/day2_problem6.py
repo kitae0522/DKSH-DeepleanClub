3bd2d8efc7a8ea28868230a01c24a71ab589f5fb
Song Kitae <40349572+kitae0522@users.noreply.github.com>
2020-04-15 12:15:02 +0900

    풀지 못한 문제

---
+++ workspace/day2/day2_problem6.py
@@ -0,0 +1,12 @@
+'''
+FileName : day2_problem6.py
+Coder : Song Ki Tae
+'''
+
+
+def sortAlpha(word):
+    pass
+
+
+if __name__ == '__main__':
+    pass
---
+++ workspace/day2/day2_problem7.py
@@ -0,0 +1,14 @@
+'''
+FileName : day2_problem7.py
+Coder : Song Ki Tae
+'''
+
+
+def prime_number(number):
+    pass
+
+
+if __name__ == '__main__':
+
+    numbers = [10, 34, 67, 245, 687, 25, 876]
+    print(prime_number(numbers))
