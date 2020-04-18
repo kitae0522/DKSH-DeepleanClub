dc0983782b8b2bcee14d88c25753504e8ebc3cc1
Song Kitae <40349572+kitae0522@users.noreply.github.com>
2020-04-18 16:36:15 +0900

    Last Update 200418

--- workspace/day2/day2_problem1.py
+++ workspace/day2/day2_problem1.py
@@ -6,21 +6,8 @@ Coder : Song Ki Tae
 
 if __name__ == '__main__':
 
-    # a = [1, 5, 7, 6, 5, 8, 3]
-    # su = 0
-    #
-    # for i in range(len(a)):
-    #     if i % 2 == 1:
-    #         su += a[i]
-    # print(su)
-
-    line_max = 8
-    for line in range(line_max + 1):
-        if line < 1:
-            print("       *", end=" ")
-        else:
-            for space in range(line):
-                print(" ", end="")
-            for star in range(line_max - line):
-                print("*", end="")
-        print()
+    print(" " * 6 + "*" + " " + "*")
+    for j in range(7, 0, -1):
+        left = " " * (7 - j) + "*" * j
+        right = "*" * j
+        print(left + " " + right)
